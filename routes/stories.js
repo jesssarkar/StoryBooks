0062db854d55f3bd3a9ff7215f5bfa3064289111
Jessica Sarkar <jessicalosee@gmail.com>
2022-08-25 19:25:11 -0500

    finishing touches

--- routes/stories.js
+++ routes/stories.js
@@ -39,6 +39,25 @@ router.get('/', ensureAuth, async (req,res) => {
     }
 })
 
+//show single story
+router.get('/:id', ensureAuth, async (req,res) => {
+  try{
+    let story = await Story.findById(req.params.id)
+      .populate('user')
+      .lean()
+
+      if(!story){
+        return res.render('error/404')
+      }
+      res.render('stories/show', {
+        story
+      })
+  } catch(err){
+    console.error(err)
+    res.render('error/404')
+  }
+})
+
 //edit page
 router.get('/edit/:id', ensureAuth, async (req, res) => {
     try {
@@ -62,5 +81,57 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
       return res.render('error/500')
     }
   })
+
+  //update stories
+  router.put('/:id', ensureAuth, async (req,res) => {
+    let story = await Story.findById(req.params.id).lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      story = await Story.findOneAndUpdate({_id:
+        req.params.id}, req.body, {new: true,
+        runValidators: true})
+        res.redirect('/dashboard')
+
+        }
+
+})
+
+//delete story
+router.delete('/:id', ensureAuth, async (req,res) => {
+  try{
+   await Story.remove({_id: req.params.id}) 
+   res.redirect("/dashboard")
+  } catch(err){
+    console.error(err)
+    return res.render('error/500')
+
+  }
+})
+
+//user stories
+router.get('/user/:userId', ensureAuth, async (req,res) => {
+  try{
+      const stories = await Story.find({
+        user:req.params.userId,
+        status: 'public'
+      })
+      .populate('user')
+      .lean()
+
+      res.render('stories/index', {
+        stories
+      })
+  } catch(err){
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
 
 module.exports = router
